22f5738000bb8aace481921fdff05b2dae1aef8a
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-22 16:25:36 +0200

    array con ojects creato.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+const Barnet = {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+};
+
+const Caroll = {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'angela-caroll-chief-editor.jpg',
+};
+
+const Gordon = {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg',
+};
+
+const Lopez = {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'angela-lopez-social-media-manager.jpg',
+};
+
+const Estrada = {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg',
+};
+
+const Ramos = {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'barbara-ramos-graphic-designer.jpg',
+};
+
+const arrayTeam = [Barnet, Caroll, Gordon, Lopez, Estrada, Ramos];
+
+console.log(arrayTeam); //per debugging.
